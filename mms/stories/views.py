6c277d4fa55c1a9d83404c380f8a97405321f2a4
Eric Sherman <ericandrewsherman@gmail.com>
2016-10-25 13:34:16 -0500

    basic views work

--- mms/stories/views.py
+++ mms/stories/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Prompt, Response
+
 def index(request):
     return HttpResponse("check out this siick index")
 
 def prompts(request, prompt_id):
-    return HttpResponse("Looking at story %s" % prompt_id)
+    prompt_list = Prompt.objects.all()
+    output = ', '.join([p.prompt_text for p in prompt_list])
+    return HttpResponse("Available Prompts: %s" % output)
+
 
 def results(request, prompt_id):
-    reponse = "Looking at %s."
-    return HttpResponse(response % prompt_id)
+    results_list = Response.objects.all()
+    output = ', '.join([p.response_text for p in results_list])
+    return HttpResponse("Available Stories: %s"  % output)
 
 def count(request, prompt_id):
-    return HttpResponse("You're submitting to %s" % prompt_id)
+    count = Response.objects.all()
+    output = ', '.join([str(p.count) for p in count])
+    return HttpResponse("Number of entries: %s" % output)
