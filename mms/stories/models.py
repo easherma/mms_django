b6125e6a9fbf42258a907c38f0f1a1558ae1454b
Eric Sherman <easherma@users.noreply.github.com>
2016-10-25 23:02:16 -0500

    Update models.py
    roughed out the User model

--- mms/stories/models.py
+++ mms/stories/models.py
@@ -8,6 +8,7 @@ class Prompt(models.Model):
     create_date = models.DateTimeField('date created')
     def __unicode__(self):
         return self.prompt_text
+    
 class Response(models.Model):
     prompt = models.ForeignKey(Prompt)
     response_text = models.CharField(max_length=200)
@@ -16,3 +17,19 @@ class Response(models.Model):
     story_date = models.DateTimeField('story date', default=timezone.now())
     def __unicode__(self):
         return self.response_text
+
+class User(models.Model):
+    # can some of these be stored as JSON/is that useful?
+    story_id = models.ForeignKey(Response)
+    row_id = #uniqueid/inncrement
+    user_id = #uniqueid/inncrement
+    user_name = models.CharField(max_length=200)
+    user_email = models.EmailField(max_length=254)
+    path_order = models.IntegerField(default=0)
+    geom(point) = #discuss
+    notes = models.CharField(max_length=200)
+    submit_date = models.DateTimeField('date submitted', default=timezone.now())
+    story_date = models.DateTimeField('story date', default=timezone.now())
+    def __unicode__(self): 
+        return self.user_id
+    
