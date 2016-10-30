c6d87ff933de786f892266a1871b4085fd05bc6e
Eric Sherman <ericandrewsherman@gmail.com>
2016-10-29 20:41:48 -0500

    new classes

--- mms/stories/models.py
+++ mms/stories/models.py
@@ -3,12 +3,13 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+#deprecated
 class Prompt(models.Model):
     prompt_text = models.CharField(max_length=200)
     create_date = models.DateTimeField('date created')
     def __unicode__(self):
         return self.prompt_text
-    
+#deprecated
 class Response(models.Model):
     prompt = models.ForeignKey(Prompt)
     response_text = models.CharField(max_length=200)
@@ -18,18 +19,23 @@ class Response(models.Model):
     def __unicode__(self):
         return self.response_text
 
+class Story(models.Model):
+    story_name = models.CharField(max_length=200)
+    story_description = models.TextField()
+    story_instructions = models.TextField()
+
 class User(models.Model):
-    # can some of these be stored as JSON/is that useful?
-    story_id = models.ForeignKey(Response)
-    row_id = #uniqueid/inncrement
-    user_id = #uniqueid/inncrement
+    story_id = models.ForeignKey(Story)
     user_name = models.CharField(max_length=200)
     user_email = models.EmailField(max_length=254)
+
+class Waypoint(models.Model):
+    geom = models.CharField(max_length=200)
+    notes = models.TextField()
     path_order = models.IntegerField(default=0)
-    geom(point) = #discuss
-    notes = models.CharField(max_length=200)
-    submit_date = models.DateTimeField('date submitted', default=timezone.now())
-    story_date = models.DateTimeField('story date', default=timezone.now())
-    def __unicode__(self): 
-        return self.user_id
-    
+
+class Submission(models.Model):
+    story_users_count = models.IntegerField(default=0)
+    user_id = models.ForeignKey(User)
+    story_id = models.ForeignKey(Story)
+    waypoint_id = models.ForeignKey(Waypoint)
