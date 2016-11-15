303513b76e20268f54df4f72a1bc71801dc4aea4
Eric Sherman <ericandrewsherman@gmail.com>
2016-11-15 14:25:33 -0600

    new migrations work

--- mms/stories/migrations/0001_initial.py
+++ mms/stories/migrations/0001_initial.py
@@ -11,20 +11,50 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Prompt',
+            name='Story',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('prompt_text', models.CharField(max_length=200)),
-                ('create_date', models.DateTimeField(verbose_name=b'date created')),
+                ('story_name', models.CharField(max_length=200)),
+                ('story_description', models.TextField()),
+                ('story_instructions', models.TextField()),
             ],
         ),
         migrations.CreateModel(
-            name='Response',
+            name='Submission',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('response_text', models.CharField(max_length=200)),
-                ('count', models.IntegerField(default=0)),
-                ('question', models.ForeignKey(to='stories.Prompt')),
+                ('story_users_count', models.IntegerField(default=0)),
+                ('story_id', models.ForeignKey(to='stories.Story')),
             ],
+        ),
+        migrations.CreateModel(
+            name='User',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('user_name', models.CharField(max_length=200)),
+                ('user_email', models.EmailField(max_length=254)),
+                ('story_id', models.ForeignKey(to='stories.Story')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Waypoint',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('geom', models.CharField(max_length=200)),
+                ('notes', models.TextField()),
+                ('lng', models.DecimalField(max_digits=8, decimal_places=5)),
+                ('lat', models.DecimalField(max_digits=8, decimal_places=5)),
+                ('path_order', models.IntegerField(default=0)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='submission',
+            name='user_id',
+            field=models.ForeignKey(to='stories.User'),
+        ),
+        migrations.AddField(
+            model_name='submission',
+            name='waypoint_id',
+            field=models.ForeignKey(to='stories.Waypoint'),
         ),
     ]
