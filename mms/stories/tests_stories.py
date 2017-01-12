17fcaf699dd7bd25b0036970f50f46ca9d444c1b
Eric Sherman <ericandrewsherman@gmail.com>
2017-01-11 22:51:02 -0600

    testing tweaks

--- mms/stories/tests_stories.py
+++ mms/stories/tests_stories.py
@@ -8,15 +8,15 @@ import factory.fuzzy
 from .models import Story
 from stories.factories import StoryFactory
 from stories.factories import UserFactory
-from django.test import Client
+#from django.test import Client
 
 #TODO factory.build() breaks with Django 1.8?
 #TODO reverse function dosent seem to work. not sure if i should use vanilla django or rest, and if the viewset affects it?
 #TODO not sure i understand best practices of setting up these seperate tests vs other test, looks like data dosen't carry over
 class StoryTests(APITestCase):
-    # def setUp(self):
-    #     owner = UserFactory.create()
-    #     story = StoryFactory.create()
+    def setUp(self):
+        # owner = UserFactory.create()
+        # story = StoryFactory.create()
     def test_read_story(self):
         print(Story.objects.all().values())
         #story = StoryFactory.create()
@@ -28,13 +28,13 @@ class StoryTests(APITestCase):
         story = StoryFactory.stub()
         owner = UserFactory.create()
         #self.data = {'name': 'My First Story', 'description': 'yourstory', 'instructions': 'put your dots on your maps and connect them with them lines', 'owner': owner}
-        self.data = {'name': 'name', 'description': 'description', 'instructions': 'put your dots on your maps and connect them with them lines'}
+        self.data = {'name': story.name, 'description': story.description, 'instructions': 'put your dots on your maps and connect them with them lines', 'owner': owner}
 
         response = self.client.post('/api/stories/', self.data)
-        print(response)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Story.objects.count(), 1)
         self.assertEqual(Story.objects.get().name, story.name)
+        print(response)
 """
 class ReadStory(APITestCase):
     def test_can_read_story(self):
